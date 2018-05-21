ee4f7b026e57a60cf6bd94dcdcb1d39b66010121
Vuze <vuze.mhf2resg@gmail.com>
2018-05-21 21:02:33 +0200

    Update ridaonline-popup-workaround.user.js

--- ridaonline-popup-workaround.user.js
+++ ridaonline-popup-workaround.user.js
@@ -1,17 +1,18 @@
 // ==UserScript==
 // @name         RidaOnline Popup Workaround
 // @namespace
-// @version      1.0
+// @version      1.1
 // @description  Modifies the "Rida PDF" button to work with pop-ups blocked (especially useful for Safari).
 // @author       Cornelius W.
-// @match        http://www.ridaonline.at/*
+// @include      http://www.ridaonline.at/*
+// @include      http://193.33.115.179/*
 // @grant        none
 // ==/UserScript==
 
 function CreatePDFLink() {
 	try {
-		pdfId = document.getElementById("RIONVOLL.V.R1.RIDANR").attributes["value"].textContent;
-		pdfUrl = "http://www.ridaonline.at/RIONPdf.a5w?PDFNr=" + pdfId + "&UserNr=63&PCNum=77777777&FundLink=";		// Modify UserNr / PCNum if necessary
+		pdfId = document.getElementById("RIONVOLL.V.R5.RIDANR").attributes["value"].textContent;
+		pdfUrl = "http://www.ridaonline.at/RidaOnline/RIONPdf.a5w?PDFNr=" + pdfId + "&UserNr=##&PCNum=########&FundLink=";		// Modify UserNr / PCNum if necessary
 		document.getElementById("ButtPDF").attributes["onclick"].textContent = "window.open('" + pdfUrl + "','_blank')";
 	}
 	catch(err) {
